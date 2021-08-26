f99c1cbb420f5daeb40def4152f3f8f8f787879f
Temi <temioye30@gmail.com>
2021-08-26 10:31:58 +0100

    working prototype

--- Quote machine.js
+++ Quote machine.js
@@ -1,15 +1,44 @@
 let quotesArr = [];
 let currentImgIndex = 0;
-let quoteText = document.getElementById("quote");
-let btn = document.getElementById("test");
+let quoteDiv = document.getElementById("quote");
+let quoteeDiv = document.getElementById("quotee");
+let animeBtn = document.getElementById("anime-btn");
+let kanyeBtn = document.getElementById("kanye-btn");
+let inspirationalBtn = document.getElementById("inspirational-btn");
+let stoicBtn = document.getElementById("stoic-btn");
+let movieBtn = document.getElementById("movie-btn");
 
-function changeQuoteText(str) {
-  quoteText.innerText = str;
-  // console.log("sup")
+function changeQuoteText(quoteText, quoteeText="Kanye", originText="") {
+  quoteDiv.innerText = `"${quoteText}"`;
+
+  let quoteHasAnOrigin = originText.length > 0;
+  let quoteHasAQuotee = quoteeText.length > 0;
+
+  if(quoteHasAnOrigin && quoteHasAQuotee){
+    quoteeDiv.innerText = `-${quoteeText} from ${originText}`;
+  } else{
+    quoteeDiv.innerText = `-${quoteeText}`;
+  }
 }
 
-btn.addEventListener("click", changeQuoteText("Sup"))
+function fetchQuote(url, quote, quotee="", origin="") {
+  fetch(url)
+      .then(response => response.json())
+      .then(quoteInfo => changeQuoteText(quoteInfo[quote], quoteInfo[quotee], quoteInfo[origin]))
+}
 
-// btn.addEventListener("click", event => {
-//   changeQuoteText("Sup");
-// });
+animeBtn.addEventListener("click", event => {
+  fetchQuote('https://animechan.vercel.app/api/random', "quote", "character", "anime");
+  });
+
+kanyeBtn.addEventListener("click", event => {
+  fetchQuote('https://api.kanye.rest', "quote")
+});
+
+movieBtn.addEventListener("click", event => {
+  fetchQuote('https://movie-quote-api.herokuapp.com/v1/quote/', "quote", "role", "show")
+});
+
+inspirationalBtn.addEventListener("click", event =>{
+  fetchQuote('https://api.quotable.io/random', "content", "author")
+});
